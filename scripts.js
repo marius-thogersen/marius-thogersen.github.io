431caaedf52516d1e0e68e7427e4a3f2b965f317
Marius <56964204+marius-thogersen@users.noreply.github.com>
2024-08-21 09:51:41 +0200

    stuff

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 let value = "Marius"
 
 console.log("Loaded "+ value);
+
+
+
+
+document.querySelector(".theme-switch").onclick = (e) => {
+    document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("light-theme");
+}
