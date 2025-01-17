065c1ad21ac67107ba41605e75981cd81252594d
marius-openminds <mt@openminds.dk>
2025-01-17 14:00:26 +0100

    darkness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",           // Main HTML file
     "./src/**/*.{vue,js,ts}", // All Vue and JavaScript files in `src`
